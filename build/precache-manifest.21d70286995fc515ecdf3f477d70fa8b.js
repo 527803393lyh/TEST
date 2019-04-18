bd6c849cede6ee6e8e3559db1b3c8e95d3cd3909
liuyunhui <527803393@qq.com>
2019-04-18 13:15:55 +0800

    shishi

---
+++ build/precache-manifest.21d70286995fc515ecdf3f477d70fa8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b0242a5909d1f1cdec768e97accd3c9",
+    "url": "/static/media/person.9b0242a5.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7de10baabde735a794c4",
+    "url": "/static/js/main.7de10baa.chunk.js"
+  },
+  {
+    "revision": "725ef62920eeca72deea",
+    "url": "/static/js/1.725ef629.chunk.js"
+  },
+  {
+    "revision": "7de10baabde735a794c4",
+    "url": "/static/css/main.627c6540.chunk.css"
+  },
+  {
+    "revision": "725ef62920eeca72deea",
+    "url": "/static/css/1.e42f2de2.chunk.css"
+  },
+  {
+    "revision": "102fce3bacfd82b7e8e235e81d48345a",
+    "url": "/index.html"
+  }
+];
